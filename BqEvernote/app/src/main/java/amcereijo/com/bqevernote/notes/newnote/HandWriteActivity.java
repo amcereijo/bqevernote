0f63c9f0f495fb094e35b4537194a8295d2cf217
angel_cereijo_medianet <angel.cereijo@medianet.es>
2014-12-15 17:01:13 +0100

    use a view to allow hand writtinng

--- BqEvernote/app/src/main/java/amcereijo/com/bqevernote/notes/newnote/HandWriteActivity.java
+++ BqEvernote/app/src/main/java/amcereijo/com/bqevernote/notes/newnote/HandWriteActivity.java
@@ -1,7 +1,9 @@
 package amcereijo.com.bqevernote.notes.newnote;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -14,6 +16,7 @@ import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.inject.Inject;
@@ -25,94 +28,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 import amcereijo.com.bqevernote.R;
+import amcereijo.com.bqevernote.notes.newnote.views.DrawView;
 import amcereijo.com.bqevernote.ocr.OCRServiceAPI;
 import roboguice.activity.RoboActivity;
 import roboguice.inject.ContentView;
 
 @ContentView(R.layout.activity_hand_write)
-public class HandWriteActivity extends RoboActivity implements View.OnTouchListener {
+public class HandWriteActivity extends RoboActivity {
 
     public final static int RESULT_CODE = 1024;
     public final static String TAG = HandWriteActivity.class.getName();
 
-    private ImageView imageView;
-    private Canvas canvas;
-    private Paint paint;
     private Bitmap bitmap;
-
-    private List<Float> points = new ArrayList<Float>();
-
+    private DrawView drawView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        imageView = (ImageView) this.findViewById(R.id.handwrite_imageview);
-
-        createPaint();
-        generateEmptyCanvas();
-
-        imageView.setOnTouchListener(this);
+        createDrawView();
     }
 
-    private void createPaint() {
-        paint = new Paint();
-        paint.setColor(Color.BLACK);
-        paint.setStrokeWidth(16);
+    private void createDrawView() {
+        drawView = new DrawView(this);
+        ((LinearLayout)findViewById(R.id.handwrite_drawview)).addView(drawView);
     }
 
-
-    public boolean onTouch(View v, MotionEvent event) {
-        int action = event.getAction();
-        switch (action) {
-            case MotionEvent.ACTION_DOWN:
-                points.add(event.getX());
-                points.add(event.getY());
-                break;
-            case MotionEvent.ACTION_MOVE:
-                points.add(event.getX());
-                points.add(event.getY());
-                break;
-            case MotionEvent.ACTION_UP:
-                points.add(event.getX());
-                points.add(event.getY());
-                float[] ps = new float[points.size()];
-                for(int i=0; i<points.size();i++){
-                    ps[i] = points.get(i);
-                }
-                canvas.drawPoints(ps, 0, ps.length - 1, paint);
-                imageView.invalidate();
-                break;
-            case MotionEvent.ACTION_CANCEL:
-                break;
-            default:
-                break;
-        }
-        return true;
-    }
-
-    private void generateEmptyCanvas(){
-        Display currentDisplay = getWindowManager().getDefaultDisplay();
+    private void createBitmap(){
+        Display currentDisplay = drawView.getDisplay();
         Point point = new Point();
         currentDisplay.getSize(point);
         bitmap = Bitmap.createBitmap(point.x, point.y,
                 Bitmap.Config.ARGB_8888);
-
-        canvas = new Canvas(bitmap);
-
-        imageView.setImageBitmap(bitmap);
     }
 
     @Inject
     private OCRServiceAPI ocrServiceAPI;
 
     public void processHandWrite(View v){
-
+        final ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setTitle(getString(R.string.title_activity_notes));
+        dialog.setMessage(getString(R.string.notes_note_handwrite_recog));
+        dialog.show();
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
                     File fTemp = File.createTempFile("canvas", ".png");
                     FileOutputStream fos = new FileOutputStream(fTemp);
+                    createBitmap();
                     bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
                     if(ocrServiceAPI.convertToText("es", fTemp.getAbsolutePath()) &&
                             ocrServiceAPI.getResponseCode() == 200){
@@ -129,6 +92,8 @@ public class HandWriteActivity extends RoboActivity implements View.OnTouchListe
                 } catch (IOException e) {
                     Log.e(TAG, e.getMessage(), e);
                     Toast.makeText(HandWriteActivity.this, "No se ha podido guardar el texto", Toast.LENGTH_LONG).show();
+                } finally {
+                    dialog.dismiss();
                 }
             }
         }).start();
@@ -136,7 +101,8 @@ public class HandWriteActivity extends RoboActivity implements View.OnTouchListe
     }
 
     public void clearHandWrite(View v){
-        generateEmptyCanvas();
+        ((LinearLayout)findViewById(R.id.handwrite_drawview)).removeAllViews();
+        createDrawView();
     }
 
     public void cancelHandWrite(View v){
---
+++ BqEvernote/app/src/main/java/amcereijo/com/bqevernote/notes/newnote/views/DrawView.java
@@ -0,0 +1,58 @@
+package amcereijo.com.bqevernote.notes.newnote.views;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import amcereijo.com.bqevernote.R;
+
+/**
+ * Created by medianet on 15/12/2014.
+ */
+public class DrawView extends View {
+    private Bitmap mBitmapBrush;
+    private Point mBitmapBrushDimensions;
+
+    private List<Point> mPositions = new ArrayList<Point>(100);
+
+    public DrawView(Context context) {
+        super(context);
+
+        mBitmapBrush = BitmapFactory.decodeResource(context.getResources(), R.drawable.brush);
+        mBitmapBrushDimensions = new Point(mBitmapBrush.getWidth(), mBitmapBrush.getHeight());
+
+        setBackgroundColor(0xffffffff);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        for (Point pos : mPositions) {
+            canvas.drawBitmap(mBitmapBrush, pos.x, pos.y, null);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_MOVE:
+                final float posX = event.getX();
+                final float posY = event.getY();
+                Point p = new Point();
+                p.set((int)posX, (int)posY);
+                mPositions.add(new Point((int)posX - mBitmapBrushDimensions.x / 2, (int)posY - mBitmapBrushDimensions.y / 2));
+                invalidate();
+        }
+
+        return true;
+    }
+}
